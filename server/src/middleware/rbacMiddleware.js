df60fae2979091fc507cd0bd6a0276f1d7745c8b
JeetShah2007 <jeetshah007ind@gmail.com>
2026-07-12 10:57:48 +0530

    Add auth and RBAC middleware

---
+++ server/src/middleware/rbacMiddleware.js
@@ -0,0 +1,10 @@
+function rbac(...allowedRoles) {
+  return (req, res, next) => {
+    if (!req.user || !allowedRoles.includes(req.user.role)) {
+      return res.status(403).json({ message: 'You do not have permission to access this resource' });
+    }
+    next();
+  };
+}
+
+module.exports = rbac;
