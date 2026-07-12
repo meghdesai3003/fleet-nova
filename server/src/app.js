441c8fa4b6c72edfdf1c629dc0598e4e9a6062e5
unknown <megh.desai3003@gmail.com>
2026-07-12 11:04:39 +0530

    Add express app configuration

--- server/src/app.js
+++ server/src/app.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/api/health', (req, res) => {
-  res.json({ status: 'ok', message: 'TransitOps API is running' });
+  res.json({ status: 'ok', message: 'FleetNova API is running' });
 });
 
 app.use('/api/auth', authRoutes);
