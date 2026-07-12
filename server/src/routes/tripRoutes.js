272b085c4868255a15cc22c94bf7d2f48396e53a
unknown <megh.desai3003@gmail.com>
2026-07-12 11:23:06 +0530

    Add trip routes

---
+++ server/src/routes/tripRoutes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const authMiddleware = require('../middleware/authMiddleware');
+const {
+  getTrips,
+  getTrip,
+  createTrip,
+  dispatchTrip,
+  completeTrip,
+  cancelTrip,
+} = require('../controllers/tripController');
+
+router.use(authMiddleware);
+
+router.get('/', getTrips);
+router.get('/:id', getTrip);
+router.post('/', createTrip);
+router.put('/:id/dispatch', dispatchTrip);
+router.put('/:id/complete', completeTrip);
+router.put('/:id/cancel', cancelTrip);
+
+module.exports = router;
